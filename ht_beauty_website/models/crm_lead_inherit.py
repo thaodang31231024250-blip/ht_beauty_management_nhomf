9511cbea2aabe602e068383af424858cca001515
thaodang31231024250-blip <thaodang.31231024250@st.ueh.edu.vn>
2026-06-01 17:11:06 +0700

    Update crm lead inherit

--- ht_beauty_website/models/crm_lead_inherit.py
+++ ht_beauty_website/models/crm_lead_inherit.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class CrmLeadInherit(models.Model):
     _inherit = 'crm.lead'
 
-    # Trường mở rộng nếu cần sau này (ví dụ: zalo, nguồn website,...)
+    # Trường mở rộng nếu cần sau này (ví dụ: zalo, nguồn website, v.v.)
     ht_source_page = fields.Char(
         string='Trang nguồn',
         help='Trang website mà khách hàng đã gửi form',
